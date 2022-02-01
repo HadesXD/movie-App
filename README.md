5f32199fc2f05f143408d386eee95d98ebb8d68f
HadesXD <stropnik.domen@outlook.com>
2022-02-01 19:11:58 +0100

    uploading promotion update

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # movie-App
+
+npm install express ejs, express, mysql
+npm install nodemon --save-dev
--- public/js/script.js
+++ public/js/script.js
@@ -1,43 +1,48 @@
 const API_URL = 'https://api.themoviedb.org/3';
+const API_MOVIE_GENERAL_PATH = 'https://api.themoviedb.org/3/movie/';
+
 const API_KEY = 'a32c49fc09517c91448d1166045ea031';
 const IMG_PATH = 'https://image.tmdb.org/t/p/w1280/';
 const API_TEST = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=a32c49fc09517c91448d1166045ea031&page=1';
 //const API_SEARCH ="https://api.themoviedb.org/3/search/movie?&api_key=a32c49fc09517c91448d1166045ea031&query=";
 const API_SEARCH ="https://api.themoviedb.org/3/search/movie?sort_by=popularity.desc&&api_key=a32c49fc09517c91448d1166045ea031&query=";
 
-const mainEL = document.getElementById('main');
+const popularMoviesEL = document.getElementById('movies-popularity');
 const formEl = document.getElementById('searchForm')
 const searchEl = document.getElementById('search');
 
 function setColor(score) {
-    if (score > 7.5) return "green";
+    if (score >= 7) return "green";
     else if (score >= 5) return "yellow";
-    else if (score > 0) "red";
-    else null;
+    else return "red";
 }
 
 async function getPopularMovies() {
+    const promotionalMaterial = [];
+
     const response = await fetch(API_TEST);
     const responseData = await response.json();
-    console.log(responseData.results[0].original_title);
+
+    for (let i = 0; i < 2; ++i) {
+        promotionalMaterial.push(responseData.results[i].backdrop_path)
+    }
 
     responseData.results.forEach(movie => {
         const container = document.createElement('div');
-        container.classList.add('movie');
+        container.id = movie.id;
+        container.classList.add('card');
+        container.setAttribute("onclick","showCard(" + container.id + ")");
         container.innerHTML = `
             <img src="${IMG_PATH + movie.poster_path}" 
                 alt="${movie.original_title}">
-            <div class="movie-info">
-                <h3>${movie.original_title}</h3>
+            <div class="card-info">
                 <span class="${setColor(movie.vote_average)}">
                 ${movie.vote_average}</span>
+                <p>${movie.original_title}</p>
             </div>`;
 
-        //console.log(movie);
-        mainEL.appendChild(container);
+        popularMoviesEL.appendChild(container);
     })
-
-    return responseData;
 }
 
 getPopularMovies();
@@ -53,33 +58,131 @@ formEl.addEventListener('submit', (e) => {
 })
 
 async function getSearchedMovies(value) {
-    mainEL.innerHTML = '';
+    popularMoviesEL.innerHTML = '';
     const response = await fetch(API_SEARCH + value);
     const responseData = await response.json();
 
     responseData.results.forEach(movie => {
         const container = document.createElement('div');
-        container.classList.add('movie');
+        container.classList.add('card');
         container.innerHTML = `
             <img src="${getPosterImg(movie.poster_path)}" 
                 alt="${movie.original_title}">
-            <div class="movie-info">
-                <h3>${movie.original_title}</h3>
+            <div class="card-info">
+                <p>${movie.original_title}</p>
                 <span class="${setColor(movie.vote_average)}">
                 ${movie.vote_average}</span>
-            </div>
-            <div class="overview">
-            <h4>Synopysis</h4>
-            ${movie.overview}
             </div>
             `;
 
         //console.log(movie);
-        mainEL.appendChild(container);
+        popularMoviesEL.appendChild(container);
     })
 }
 
 function getPosterImg(poster_path){
     if (poster_path) return IMG_PATH + poster_path;
-    else return "/assets/images/noPoster.jpg";
+    else return "../images/noPoster.jpg";
+}
+
+
+
+async function showCard(cardID) {
+    let card = document.getElementById('card-info');
+    card.innerHTML = "";
+    
+    const response1 = await fetch(API_MOVIE_GENERAL_PATH + cardID + "?api_key=" + API_KEY + "&language=en-US");
+    const response2 = await fetch(API_MOVIE_GENERAL_PATH + cardID + "/credits?api_key=" + API_KEY + "&language=en-US");
+    console.log(API_MOVIE_GENERAL_PATH + cardID + "/credits?api_key=" + API_KEY + "&language=en-US")
+
+    const movieData = await response1.json();
+    const creditsData = await response2.json();
+
+    const container = document.createElement('div');
+    container.classList.add('movie');
+
+    let movieGenres = "";
+    movieData.genres.forEach(genre => {
+        movieGenres += genre.name + ', ';
+        }); 
+    movieGenres = movieGenres.replace(/,\s*$/, "");
+
+    let movieDirector = "";
+    let movieWriters = "";
+    let movieActors = "";
+    
+    for (let i = 0; i < 3; ++i) {
+        movieActors += creditsData.cast[i].original_name + ", ";
+    }; movieActors = movieActors.replace(/,\s*$/, "");
+
+
+    creditsData.crew.forEach(member => {
+        if (member.job === "Director") movieDirector += member.original_name + ", ";
+        if (member.job === "Writer") movieWriters += member.original_name + ", ";
+    });
+    
+    movieDirector = movieDirector.replace(/,\s*$/, "");
+    movieWriters = movieWriters.replace(/,\s*$/, "");
+
+    container.innerHTML = `
+    <div class="menu"><i class="fas fa-times" onclick="closeButton()"></i></div> 
+    <div class="movie-img" style="background-image: url('${IMG_PATH + movieData.backdrop_path}')"></div>
+        <div class="text-movie-cont">
+        <div class="mr-grid">
+        <div class="col1">
+            <h1>${movieData.original_title}</h1>
+            <ul id="genres" class="movie-gen">
+            <li>PG-13 /</li>
+            <li>${movieData.runtime} mins /</li>
+            <li>${movieGenres}</li>
+            </ul>
+        </div>
+        </div>
+        <div class="mr-grid summary-row">
+        <div class="col2">
+            <h5>SUMMARY</h5>
+        </div>
+        </div>
+        <div class="mr-grid">
+        <div class="col1">
+            <p class="movie-description">${movieData.overview}</p>
+        </div>
+        </div>
+        <div class="mr-grid actors-row">
+        <div class="col1">
+            <div class="movie-credit-type">Director(s):
+            <p class="movie-credit-induvidual">${ movieDirector }</p></div>
+            <div class="movie-credit-type">Writers:
+            <p class="movie-credit-induvidual">${ movieWriters }</p></div>
+            <div class="movie-credit-type">Actors:
+            <p class="movie-credit-induvidual">${ movieActors }</p></div>
+        </div>
+        <div class="mr-grid action-row">
+        <div class="col2"><div class="watch-btn"><h3><i class="fas fa-play"></i>WATCH TRAILER</h3></div></div>
+        <div class="col6 action-btn"><i class="fas fa-heart"></i></div>
+        <div class="col6 action-btn"><i class="fas fa-bookmark"></i></div>
+        <div class="col6 action-btn"><i class="fas fa-star"></i></div>
+        </div>
+        </div>
+    </div>`;
+
+    console.log("the content: " + container.innerHTML);
+
+    
+    document.body.style.background = 'linear-gradient(rgba(30,27,38, 0.95), rgba(30,27,38, 0.95)), url("'+ IMG_PATH + movieData.backdrop_path +'")';
+    document.body.style.backgroundPostion = 'center';
+    document.body.style.backgroundSize = 'cover';
+    document.body.style.backgroundRepeat = 'repeat';
+
+    card.appendChild(container);
+    card.classList.remove('hidden');
+
+    let dp = document.getElementById('general-display');
+    dp.classList.add('hidden');
+}
+
+function closeButton() {
+    let card = document.getElementById('card-info');
+    card.classList.add('hidden');
+
 }
